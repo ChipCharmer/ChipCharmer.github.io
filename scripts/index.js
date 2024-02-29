c125081c76113f562b03ee91a68f07ec370683bb
PreposterousName <jestrada.je622@icloud.com>
2024-02-29 16:56:42 -0600

    Commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    fetch('/text/homepage1.txt')  
+    fetch('/scripts/text/homepage1.txt')  
     .then(response => response.text())  
     .then(text => {  
-    const paragraphPlaceholder = document.getElementById('paragraphPlaceholder');
-    paragraphPlaceholder.innerText = text;  
+        const paragraphPlaceholder = document.getElementById('paragraphPlaceholder');
+        paragraphPlaceholder.innerText = text;  
     })
     .catch(error => console.error('Error fetching paragraph:', error));  
 }); 
