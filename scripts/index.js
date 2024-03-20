565a108d82d0306f0f8c91adaa00e617442d28c0
ChipCharmer <88461623+ChipCharmer@users.noreply.github.com>
2024-03-20 17:37:25 -0600

    New version of website

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/text/homepage1.txt')  
-    .then(response => response.text())  
-    .then(text => {  
-        const paragraphPlaceholder = document.getElementById('paragraphPlaceholder');
-        paragraphPlaceholder.innerText = text;  
-    })
-    .catch(error => console.error('Error fetching paragraph:', error));  
-}); 
